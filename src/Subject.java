d56c29d9ba45c7805878affc475f3d80fedab473
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-31 10:26:59 -0300

    Update Subject.java

--- src/Subject.java
+++ src/Subject.java
@@ -1,17 +1,27 @@
 import java.util.*;
 
 public class Subject {
-    private ArrayList<Integer> angles = new ArrayList<>();
+    public ArrayList<Integer> angles = new ArrayList<>();
+    public int generation;
+    public double fitness;
 
     public Subject(ArrayList<Integer> angles) {
         this.angles = angles;
     }
 
-    public ArrayList<Integer> getAngles() {
-        return angles;
+    static Subject generateSubject(){
+        ArrayList<Integer> angles = new ArrayList<>();
+        Random generator = new Random();
+
+        for(int i = 0; i < 6; i++){
+            angles.add(generator.nextInt(360) + 1);
+        }
+        return new Subject(angles);
     }
 
-    public void setAngles(ArrayList<Integer> angles) {
-        this.angles = angles;
+    static void generatePopulation(ArrayList<Subject> population, int n){
+        for(int i = 0; i < n; i++){
+            population.add(generateSubject());
+        }
     }
 }
