258840f7ce00027f4cdebcfcf16d338c2a216e3b
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-31 10:46:23 -0300

    Update Util.java

--- src/Util.java
+++ src/Util.java
@@ -1,12 +1,41 @@
 import java.util.*;
+import java.net.*;
+import java.io.*;
 
 public class Util {
 
-    static String setUrl(ArrayList<Integer> angles){
+    public static String setUrl(ArrayList<Integer> angles){
          return "http://localhost:8080/antenna/simulate?phi1=" + Integer.toString(angles.get(0)) +
                 "&theta1=" + Integer.toString(angles.get(1)) + "&phi2=" + Integer.toString(angles.get(2)) +
                 "&theta2=" + Integer.toString(angles.get(3)) + "&phi3=" + Integer.toString(angles.get(4)) +
                 "&theta3=" + Integer.toString(angles.get(5));
+    }
+    //Função ainda teste
+    public static void accessUrl(String url){
+
+        HttpURLConnection connection = null;
+        OutputStreamWriter wr = null;
+        BufferedReader rd = null;
+        StringBuilder sb = null;
+        String line = null;
+
+        URL serverAddress = null;
+
+        try{
+            serverAddress = new URL(url);
+            connection = (HttpURLConnection)serverAddress.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setReadTimeout(10000);
+
+            connection.connect();
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
 }
