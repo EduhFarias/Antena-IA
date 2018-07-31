eb63b19751af863b84f68caa5a546437590f36d1
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-31 15:00:28 -0300

    Update Util.java

--- src/Util.java
+++ src/Util.java
@@ -5,21 +5,20 @@ import java.io.*;
 public class Util {
 
     public static String setUrl(ArrayList<Integer> angles){
-         return "http://localhost:8080/antenna/simulate?phi1=" + Integer.toString(angles.get(0)) +
+        return "http://localhost:8080/antenna/simulate?phi1=" + Integer.toString(angles.get(0)) +
                 "&theta1=" + Integer.toString(angles.get(1)) + "&phi2=" + Integer.toString(angles.get(2)) +
                 "&theta2=" + Integer.toString(angles.get(3)) + "&phi3=" + Integer.toString(angles.get(4)) +
                 "&theta3=" + Integer.toString(angles.get(5));
     }
     //Função ainda teste
-    public static void accessUrl(String url){
+    public static double accessUrl(String url){
 
         HttpURLConnection connection = null;
-        OutputStreamWriter wr = null;
         BufferedReader rd = null;
         StringBuilder sb = null;
         String line = null;
-
         URL serverAddress = null;
+        double fitness = 0;
 
         try{
             serverAddress = new URL(url);
@@ -30,12 +29,27 @@ public class Util {
 
             connection.connect();
 
+            rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            sb = new StringBuilder();
+
+            while((line = rd.readLine()) != null){
+                sb.append(line + '\n');
+            }
+            //System.out.println(sb.length());
+            //System.out.println(sb.toString());
+
+            String aux = sb.toString().substring(0,19);
+            aux = aux.replace('p','\0');
+            fitness = Double.parseDouble(aux);
+            System.out.println(fitness);
+
+            connection.disconnect();
         }catch (MalformedURLException e){
             e.printStackTrace();
         }catch (IOException e){
             e.printStackTrace();
         }
-
+        return fitness;
     }
 
 }
