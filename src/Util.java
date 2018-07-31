594fc84e5bc6807009cb5d754c004702a71c14b0
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-31 09:55:45 -0300

    Update Util.java

--- src/Util.java
+++ src/Util.java
@@ -1,24 +1,12 @@
 import java.util.*;
 
 public class Util {
-    static void urlBase(ArrayList<String> url){
-        url.add("http://localhost:8080/antenna/simulate?phi1=");
-        url.add("&theta1=");
-        url.add("&phi2=");
-        url.add("&theta2=");
-        url.add("&phi3=");
-        url.add("&theta3=");
-    }
 
-    static String setUrl(ArrayList<String> url, ArrayList<Integer> angles){
-        String out = new String();
-        int angle;
-        for(int i = 0; i < 6; i++){
-            out += url.get(i);
-            angle = angles.get(i);
-            out += Integer.toString(angle);
-        }
-        return out;
+    static String setUrl(ArrayList<Integer> angles){
+         return "http://localhost:8080/antenna/simulate?phi1=" + Integer.toString(angles.get(0)) +
+                "&theta1=" + Integer.toString(angles.get(1)) + "&phi2=" + Integer.toString(angles.get(2)) +
+                "&theta2=" + Integer.toString(angles.get(3)) + "&phi3=" + Integer.toString(angles.get(4)) +
+                "&theta3=" + Integer.toString(angles.get(5));
     }
 
     static void getIn(ArrayList<Integer> angles){
