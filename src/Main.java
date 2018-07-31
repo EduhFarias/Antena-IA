eb7d4c718eb38ef878d5d2666f0752347e171377
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2018-07-31 09:55:23 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,16 @@ import java.io.*;
 public class Main {
     public static void main(String[] args){
 
-        ArrayList<String> url = new ArrayList<>();
-        Util.urlBase(url);
-
         ArrayList<Integer> angles = new ArrayList<>();
         Util.getIn(angles);
 
         Subject newSubject = new Subject(angles);
 
-        String out = Util.setUrl(url, newSubject.getAngles());
+        String out = Util.setUrl(newSubject.getAngles());
 
         System.out.println(out);
+
+
     }
 
 }
